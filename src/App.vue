f0d335c53ad034002a066ca3bc44acc0bee965bf
Jesse Khorasanee <jessekhorasanee@gmail.com>
2019-11-28 16:26:13 +1300

    Added a rough implementation of csv loading and resizing

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,106 @@
 <template>
-  <div id="app">
-    <vue-plotly :data="data" :layout="layout" :options="options"/>
+  <div id="app" ref="app">
+    <vue-plotly
+      id="chart"
+      ref="plotly"
+      :data="data"
+      :layout="layout"
+      :options="options"
+      :autoResize="true"
+      @hover="layout.title='hovering on plotly'"
+
+    />
     <dat-gui closeText="Close controls" openText="Open controls" closePosition="bottom">
       <dat-string v-model="layout.title" label="Title" />
-      <dat-number v-model="data[0].y[0]" :min="-100" :max="100" :step="1" label="Adjust y1"/>
-      <dat-number v-model="data[0].y[1]" :min="-100" :max="100" :step="1" label="Adjust y2"/>
-      <dat-number v-model="data[0].y[2]" :min="-100" :max="100" :step="1" label="Adjust y3"/>
-      <dat-number v-model="data[0].y[3]" :min="-100" :max="100" :step="1" label="Adjust y4"/>
+      <dat-string v-model="url" label="Load CSV" />
+      <dat-string v-model="emailAddress" label="Load CSV" />
+      <dat-string v-model="twitterHandle" label="Load CSV" />
+      <dat-string v-model="instagram" label="Load CSV" />
     </dat-gui>
+    <button
+      id="external-button"
+      @mouseover="hoverd()"
+      @click="calculateWidth()"
+    >Resize Plot</button>
     <h3>title: {{layout.title}}</h3>
     <h3>y array: {{data[0].y}}</h3>
+    <h3>{{loadURL}}</h3>
+    <div :testy="testy">gg{{testy}}</div>
+    <contact-info
+      :email-address="emailAddress"
+      :twitter-handle="twitterHandle"
+      :instagram="instagram"
+    />
   </div>
 </template>
-
 <script>
-import VuePlotly from '@statnett/vue-plotly'
+import VuePlotly from "@statnett/vue-plotly";
 import Vue from "vue";
 import DatGui from "@cyrilf/vue-dat-gui";
+import ContactInfo from "./contact_info";
+import CsvManager from "./csv_manager";
+var csv = new CsvManager();
+window.VuePlotly = VuePlotly;
 Vue.use(DatGui);
-
-export default {
+var hi = "hi";
+var exx = {
   name: "app",
-  components: { VuePlotly },
+  components: { VuePlotly, ContactInfo },
+  props: ["testy"],
   data: function() {
     return {
-      x: [1,2,3,4],
-      y: [10, 15, 13, 17],
-      msg: "Welcome to Your Vue.js App",
       data: [{ x: [1, 2, 3, 4], y: [10, 25, 20, 50] }],
-      layout: {title:'edit this title'},
-      options: {}
+      layout: {
+        title: "edit this title"
+      },
+      autoResize: true,
+      options: {},
+
+      url:
+        "https://mapcore-bucket1.s3-us-west-2.amazonaws.com/ISAN/csv-data/stellate/sample_2/cell_1/18525003_channel_1.csv",
+      emailAddress: "john@smith.com",
+      twitterHandle: "johnsmith",
+      instagram: "johnsmith345"
     };
   },
+  computed: {
+    loadURL: function() {
+      csv.loadFile(this.url).then(_ => {
+        this.data[0].x = csv.getColoumnByIndex(0);
+        this.data[0].y = csv.getColoumnByIndex(1);
+      });
+    },
+    initialiseResizeListener: function(resizeObject) {
+      resizeObject.addEventListener("resize", _ => {
+        VuePlotly.relayout(chartDiv, {
+          width: resizeObject.innerWidth,
+          height: resizeObject.innerHeight + 150
+        });
+      });
+    },
+  },
+  methods: {
+        hoverd: function() {
+      this.layout.title = "We are hovering!!";
+    },
+    calculateWidth: function (){
+      this.layout.title = this.$refs.app.clientWidth
+      this.$refs.plotly.relayout({width: this.$refs.app.clientWidth, })
+      window.reeefs = this.$refs
+    }
+  }
 };
+
+var resize = function(resizeObject) {
+  resizeObject.addEventListener("resize", _ => {
+    VuePlotly.relayout(chartDiv, {
+      width: resizeObject.innerWidth,
+      height: resizeObject.innerHeight + 150
+    });
+  });
+};
+window.exx = exx;
+export default exx;
 </script>
 
 <style>
